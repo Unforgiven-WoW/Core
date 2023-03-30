afd7ded4da1e4f8db401a955c252479eaadd6381
Snuffish <snuffish90@gmail.com>
2023-03-30 23:05:52 +0200

    wip

---
+++ create-account/src/pages/components/RegisterForm/index.tsx
@@ -0,0 +1,106 @@
+import { Box, Button, Grid, TextField } from "@mui/material";
+import './styles.module.css'
+import validate from 'validate.js'
+import { useEffect, useRef, useState } from "react";
+
+const constraints = {
+    username: {
+        length: {
+            minimum: 3,
+            maximum: 32,
+            message: 'The requested account name is to short. Please try again.',
+        },
+    },
+    password: {
+        length: {
+            minimum: 6,
+            maximum: 32,
+            message: 'The requested account name is to short. Please try again.',
+        }
+    },
+    email: {
+        email: {
+            message: 'The requested email is not valid. Please try again.',
+        }
+    }
+}
+
+export default function RegisterForm() {
+    const usernameRef = useRef()
+    const passwordRef = useRef()
+    const confirmPasswordRef = useRef()
+    const emailRef = useRef()
+
+    const validateForm = ({ target }: any) => {
+        const id = target.id
+        const value = target.value
+
+        const formData = {
+            // @ts-ignore
+            username: usernameRef.current?.value,
+            // @ts-ignore
+            password: passwordRef.current?.value,
+            // @ts-ignore
+            email: emailRef.current?.value
+        }
+
+        const errors = validate(formData, constraints)
+        for (const field in errors) {
+            // @ts-ignore
+            usernameRef.current.setAttribute('error', true)
+            console.log("DSDS")
+        }
+
+        // @ts-ignore
+        usernameRef.current.setAttribute('error', true)
+    }
+
+    return (
+        <Grid alignItems='center' style={{
+            backgroundColor: 'white',
+            padding: '25px'
+        }}>
+            <Grid item>
+                <TextField
+                    id='username'
+                    label='Username'
+                    inputRef={usernameRef}
+                    onChange={validateForm}
+                />
+            </Grid>
+            <Grid item>
+                <TextField
+                    id='password'
+                    label='Password'
+                    type='password'
+                    inputRef={passwordRef}
+                    onChange={validateForm}
+                />
+            </Grid>
+            <Grid item>
+                <TextField
+                    id='confirmPassword'
+                    label='Confirm password'
+                    inputRef={confirmPasswordRef}
+                    onChange={validateForm}
+                />
+            </Grid>
+            <Grid item>
+                <TextField
+                    id='email'
+                    label='Email'
+                    inputRef={emailRef}
+                    onChange={validateForm}
+                />
+            </Grid>
+            <Grid item>
+                <Button
+                    className='registerButton'
+                    variant='contained'
+                    id='registerButton'>
+                    Register
+                </Button>
+            </Grid>
+        </Grid>
+    )
+}
