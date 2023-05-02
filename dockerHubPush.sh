ec7aa1ac51160be84617929de21c1e42ebfc2f0c
Snuffish <snuffish90@gmail.com>
2023-05-03 01:53:12 +0200

    fix

--- dockerHubPush.sh
+++ dockerHubPush.sh
@@ -5,6 +5,7 @@ read -p "Enter release version: " VERSION
 IMAGE_NAME="unforgivenwow/core:$VERSION"
 
 read -p "Do you want to create and push the image \"$IMAGE_NAME\" ? (y/n): " answer; [[ $answer =~ ^(y|Y) ]] && \
-    docker tag unforgivenwow/core:latest $IMAGE_NAME && \
+    docker tag core-worldserver:latest $IMAGE_NAME && \
+    docker tag core-authserver:latest $IMAGE_NAME && \
     docker push $IMAGE_NAME \
 || exit
